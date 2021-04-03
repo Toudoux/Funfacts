9ee150ec511748002719783cab7eded9eee600b9
Toudoux <70871731+Toudoux@users.noreply.github.com>
2021-04-03 00:00:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ function myfunction(){
+      document.getElementById("dabide").innerHTML = "DABIDE IS MEXICAN" ;
+      var img = document.createElement('img');
+document.getElementById("pic").style.opacity = "1";
+    }
